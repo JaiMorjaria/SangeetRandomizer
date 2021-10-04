c968b51bdb8ef103e125b51ec198bf46ad4ca723
Jai Morjaria <jaimorjaria2099@gmail.com>
2021-10-05 01:59:38 +0300

    test again

--- frontend/src/components/People.js
+++ frontend/src/components/People.js
@@ -5,7 +5,7 @@ export const People = ({people}) => {
 
     for (let i = people.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [people[i], people[j]] = [people    [j], people[i]];
+        [people[i], people[j]] = [people[j], people[i]];
     }
     return (
         <List ordered>
